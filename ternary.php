973ba5ca6f48f39b05773ee48b48904f2e475c6d
Waqar <waqarrao300@gmail.com>
2019-05-25 22:52:26 +0500

    ternary core php

---
+++ ternary.php
@@ -0,0 +1,16 @@
+<?php
+ 
+$result = null;
+ 
+if (5>3) {
+    $result = "Bigger";
+} else {
+    $result = "Less";
+}
+
+?>
+
+// <?php
+// // ternary operator
+// $result = 5 > 3 ? "Bigger" : "Less";
+// ?>
