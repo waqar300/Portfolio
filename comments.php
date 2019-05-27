fc4189c5ec8f170375ee23eb2192a272e16ff3cb
Waqar <waqarrao300@gmail.com>
2019-05-27 12:26:39 +0500

    comment

---
+++ comments.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment
+that can spans over multiple
+lines
+*/
+
+// You can also use comments to leave out a parts of a code
+$i = 5 /* change the value + 55 */ + 5;
+echo $i;
+?>
+
+</body>
+</html>
