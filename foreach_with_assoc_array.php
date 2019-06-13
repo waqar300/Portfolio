9e8f3747d7a3edaa427efd8212a3b2595991c7d0
Waqar <waqarrao300@gmail.com>
2019-06-13 23:21:11 +0500

    foreach

---
+++ foreach_with_assoc_array.php
@@ -0,0 +1,15 @@
+<?php
+$animals = array(
+    'vulture'=>'screams',
+    'bat'=>'screeches',
+    'human'=>'talk',
+    'bear'=>'roars',
+    'dolphin'=>'clicks'
+);
+foreach ($animals as $animal => $sound) {
+    echo "animals sounds ".$animal." ".$sound."</br>";
+}
+
+
+
+?>
