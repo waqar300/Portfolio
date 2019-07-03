87ab3306048e8d304728f4062f2f553ef45a451c
waqar300 <waqarrao300@gmail.com>
2019-07-03 11:09:15 +0500

    php core

---
+++ null-parameter-function.php
@@ -0,0 +1,8 @@
+   <?php
+         function printMe($param = NULL) {
+            print $param;
+         }
+         //null parameter no need to pass or define it
+         printMe("This is test");
+         printMe();
+      ?>
